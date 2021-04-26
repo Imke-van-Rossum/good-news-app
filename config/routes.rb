35f68d4632a23d5bc5d70ade554bf00f7db97d42
Imke van Rossum <imkevanrossum@hotmail.com>
2021-04-26 10:39:18 +0200

    add api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :articles, only: %i[index show]
+
+  namespace :api do
+    resources :articles, only: %i[create]
+  end
 end
