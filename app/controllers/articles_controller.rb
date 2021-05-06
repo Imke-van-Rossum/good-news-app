7d2af153c7df9771fbb5dbb1c0f41b0df3b1d37d
Julian Wakefield <julianwakefield@outlook.com>
2021-05-06 11:55:55 +0100

    controller actions in progress

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,41 @@
 class ArticlesController < ApplicationController
+  # before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @Article = Article.new
   end
 
   def create
-    # Xavier API scraping Data
+    @article = Article.new(article_params)
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+  
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :media, :content, :date, :news_org_name, :article_url, :author_name)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,9 @@ puts "----------------------------------"
 puts "creating articles"
 
 Article.create(title: 'title', media: 'url', content: 'text', date: Time.new.to_s, news_org_name: 'name', article_url: 'url', author_name: 'author name')
+Article.create(title: 'title2', media: 'url2', content: 'text2', date: Time.new.to_s, news_org_name: 'name2', article_url: 'url2', author_name: 'author name2')
+Article.create(title: 'title3', media: 'url3', content: 'text3', date: Time.new.to_s, news_org_name: 'name3', article_url: 'url3', author_name: 'author name')
+Article.create(title: 'title4', media: 'url4', content: 'text4', date: Time.new.to_s, news_org_name: 'name4', article_url: 'url4', author_name: 'author name2')
 
 puts "----------------------------------"
 puts "#{Article.count} Articles created"
